5700581d5f3738076c78f561ae8586cb7f4c9cad
Bryan Chu <bryan@Bryans-MacBook-Pro.local>
2016-07-23 16:51:23 -0400

    updated error test

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 'use strict';
 
-module.exports = function(fn){
+module.exports = function (fn){
   if(!fn || typeof fn !== 'function') throw new TypeError('error: dePromisify must be called with a function');
   return function (...args){
+    const originalArgs = args.slice(0);
     const cb = args.pop();
-    Promise
-    .resolve(fn)
-    .then(trustedFn=>{
-      trustedFn(...args);
-      try {
-        return trustedFn(...args);
+    const isFunction = (typeof cb === 'function');
+    if(!isFunction) {
+      console.info('last argument is not a callback function, calling original arguments')
+      try{
+        return fn(...originalArgs);
       } catch (e) {
-        return new TypeError('error: the last argument is not a function');
+        console.error(e.stack);
       }
-    })
-    .then(data=>{
-      console.log('TEST', data);
-      cb(null, data)
-    })
-    .catch(err=>cb(err));
-  };
-}
+    } else {
+      Promise
+      .resolve(fn)
+      .then(trustedFn=>trustedFn(...args))
+      .then(data=>cb(null, data))
+      .catch(err=>cb(err));
+    }
+  }
+};
--- test/test_spec.js
+++ test/test_spec.js
@@ -4,7 +4,7 @@ require('mocha');
 const path = require('path');
 const chai = require('chai');
 const expect = chai.expect;
-const de_promisify = require(path.resolve(__dirname, '../index'));
+const depromisify = require(path.resolve(__dirname, '../index'));
 const fs = require('fs');
 const bluebird = require('bluebird');
 
@@ -21,8 +21,10 @@ describe('de-promisfy', function (){
     });
   });
   describe('functionality', function (){
+    const readFileAsync = bluebird.promisify(fs.readFile);
+    const testAsync = depromisify(readFileAsync);
     it('Should be an function', function (){
-      expect(typeof de_promisify === 'function').to.be.true;
+      expect(typeof depromisify === 'function').to.be.true;
     });
     it('Should throw an error when called with a non-object', function (){
       try {
@@ -32,15 +34,33 @@ describe('de-promisfy', function (){
         expect(e instanceof Error).to.be.true;
       }
     });
-    it('Should accept a callback as the last argument', function (done){
+    it('Should run last argument as a callback', function (done){
       this.timeout(7000);
-      const readFileAsync = bluebird.promisify(fs.readFile);
-      const testAsync = de_promisify(readFileAsync);
-
       testAsync(path.resolve(__dirname, './sample.txt'), 'utf8', function (err, data){
-        if(err) done(err);
-        else(data === successData) ? done() : done(new Error('text is not the same'))
-      })
+        if(!err) {
+          expect(data).to.equal(successData);
+          done();
+        } else {
+          done(err);
+        }
+
+      });
     });
+    it('Should handle error if last argument is a callback', function (done){
+      this.timeout(7000);
+      testAsync(path.resolve(__dirname, './doesNotExist.txt'), 'utf8', function (err, data){
+          expect(err).to.be.instanceOf(Error);
+          done();
+      });
+    });
+
+    it('Should call original function if last argument is not a callback function', function(done){
+        const sillyFunc = function(a,b,c){
+            return a+b+c;
+        };
+        const depromsifySillyFunc = depromisify(sillyFunc);
+        expect(depromsifySillyFunc(1,2,3)).to.equal(6);
+      done();
+    })
   });
 });
